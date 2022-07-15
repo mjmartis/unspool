85624dc283d067eb3ed5a088d958771d42baf1fe
Michael Martis <michaeljmartis@gmail.com>
2022-07-15 18:02:52 +1000

    Nicer console output

--- unspool.sh
+++ unspool.sh
@@ -15,7 +15,7 @@ readonly ext="${in_fn#*.}"
 
 # First, split audio into chunks and save the number of
 # chunks extracted.
-echo "Splitting original audio into segments."
+echo "[...] Splitting original audio into segments."
 readonly seg_count=$(
   ffmpeg -i "${in_fn}" \
          -f segment -segment_time ${SEG_LENGTH_SEC} \
@@ -24,26 +24,31 @@ readonly seg_count=$(
          2>&1 | \
   grep "Opening .* for writing" -c -
 )
+echo -e '\e[1A\e[K[Done] Splitting original audio into segments.'
 
 # File to output to, since we can't read and write to
 # the same file.
 readonly tmp_fn="$(mktemp --suffix ".${ext}")"
 
 # Second, prepend chunk description audio to each file.
+echo ''
 for (( i=0; i<${seg_count}; ++i )); do
-  echo "Prepending description to segment $((i+1)) of ${seg_count}."
-
   # The current chunk to read from / write to.
-  cur_fn="$(printf "${in_dir}/${out_prefix}_%03d.${ext}" $i)"
+  cur_fn=$(printf "${out_prefix}_%03d.${ext}" $i)
+  cur_path="${in_dir}/${cur_fn}"
+
+  echo -e "\e[1A\e[K[$((i+1))/${seg_count}] Prepending description to ${cur_fn}."
 
   # Generate description then concatenate it to chunk. Writes
   # to our temporary file.
   gtts-cli "${title}. part $((i+1)) of ${seg_count}." | \
-  ffmpeg -y -i - -i "${cur_fn}" \
+  ffmpeg -y -i - -i "${cur_path}" \
          -filter_complex "[0:a][1:a]concat=n=2:v=0:a=1[a]" \
          -map "[a]" "${tmp_fn}" \
          2>/dev/null
 
   # Overwrite chunk file with the copy that has a description.
-  cp -f "${tmp_fn}" "${cur_fn}"
+  cp -f "${tmp_fn}" "${cur_path}"
 done
+
+echo -e "\e[1A\e[K[Done] Prepending descriptions to segments."
